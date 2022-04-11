407c073c4f6e94cca7461a6904eb0ca2e24df50c
Obiora Igwilo <igwiloobiora@outlook.com>
2022-04-11 14:55:15 +0100

    Add project files.

---
+++ GenericConstraint/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GenericConstraint
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var shoppingBag = new ShoppingBag<Bag>();
+            shoppingBag.AddItem(new Bag() { Name = "Indomie", Count = 45});
+            shoppingBag.AddItem(new Bag() { Name = "Indomie", Count = 30 });
+            var counter = 0;
+            while (counter < 5)
+            {
+                Console.WriteLine(shoppingBag.GetItem(counter));
+                counter++;
+            }
+            shoppingBag.GetItem(2);
+        }
+
+    }
+
+    public class ShoppingBag<T> where T : Bag 
+    {
+        public T[] items = new T[5];
+        int _index = 0;
+
+        public void AddItem(T bags)
+        {
+            items[_index++] = bags;
+        }
+
+        public T GetItem(int index)
+        {
+            return items[_index];
+        }
+    }
+
+    public class Bag
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nCount: {Count}";
+        }
+    }
+  
+     
+    
+
+}
